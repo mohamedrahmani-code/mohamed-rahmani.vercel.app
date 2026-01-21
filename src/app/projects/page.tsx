cc38c9fa23a4c0e17fd251287f55880516e47bd2
mohamedrahmani-code <mohamed.rahmani@edu.univ-paris13.fr>
2026-01-21 21:43:44 +0100

    update projects page title

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -8,7 +8,7 @@ export default async function ProjectsPage() {
   const personal_projects = projectSchema.parse(data2).projects;
   return (
     <article className="mt-8 flex flex-col gap-8 pb-16">
-      <h1 className="text-5xl">Mes projects</h1>
+      <h1 className="text-5xl">My projects</h1>
       <section className="grid grid-cols-1 gap-4 sm:grid-cols-2">
         {student_projects.map((project, id) => (
           <ProjectCard key={id} project={project} />
